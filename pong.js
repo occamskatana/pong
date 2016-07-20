8d878ed4e7acbf2fa324b8f47cfe1b72e3437d85
john <johngallweycarter@gmail.com>
2016-07-20 16:20:17 -0400

    refactoring

--- pong.js
+++ pong.js
@@ -1,65 +1,64 @@
-var setElements = function(){
+
+function Table(positionx, positiony){
+	this.positionx = positionx;
+	this.positiony = positiony;
+} 
+
+
+function Paddle(positionx, positiony){
+	this.positionx = positionx;
+	this.positiony = positiony;
+}
+
+function Ball(positionx, positiony){
+	this.positionx = positionx;
+	this.positiony = positiony;
+}
+
+Table.prototype.render = function(positionx, positiony, context, canvasWidth, canvasHeight){
+	context.beginPath();
+	context.rect(positionx, positiony, canvasWidth, canvasHeight);
+	context.fill();
+	context.lineWidth = 5;
+	context.strokeStyle = "green";
+	context.stroke();
+}
+
+Paddle.prototype.render = function (positionx, positiony, context) {
+	context.fillStyle="white";
+	context.fillRect(positionx, positiony, 15, 85);
+}
+
+Ball.prototype.render = function(positionx, positiony, context, canvasWidth, canvasHeight){
+	context.beginPath();
+	context.arc(positionx, positiony, 8, 0, 2 * Math.PI, false)
+	context.fiillStyle="white";
+	context.fill();
+}
+
+var render = function(context, canvasWidth, canvasHeight){
+	table.render(0, 0, context, canvasWidth, canvasHeight);
+	cpuPaddle.render(2, 100, context);
+	playerPaddle.render(canvasWidth - 17, 220, context)
+	ball.render(canvasWidth / 2, canvasHeight /  2, context);
+}
+
+ window.onload = function(){
 
 	var canvas = document.getElementById('canvas');
 	var context = canvas.getContext('2d');
 	var canvasHeight = context.canvas.clientHeight;
 	var canvasWidth = context.canvas.clientWidth;
-
-	function Table(positionx, positiony){
-		this.positionx = positionx;
-		this.positiony = positiony;
-	} 
-
-
-	function Paddle(positionx, positiony){
-		this.positionx = positionx;
-		this.positiony = positiony;
-	}
-
-	function Ball(positionx, positiony){
-		this.positionx = positionx;
-		this.positiony = positiony;
-	}
-
-	Table.prototype.render = function(positionx, positiony){
-		context.beginPath();
-		context.rect(positionx, positiony, canvasWidth, canvasHeight);
-		context.fill();
-		context.lineWidth = 5;
-		context.strokeStyle = "green";
-		context.stroke();
-	}
-
-	Paddle.prototype.render = function (positionx, positiony) {
-		context.fillStyle="white";
-		context.fillRect(positionx, positiony, 15, canvasHeight/4);
-	}
-
-	Ball.prototype.render = function(positionx, positiony){
-		context.beginPath();
-		context.arc(positionx, positiony, 8, 0, 2 * Math.PI, false)
-		context.fiillStyle="white";
-		context.fill();
-	}
-
-
-  var table = new Table();
-  table.render(0, 0);
+  table = new Table();
   cpuPaddle = new Paddle(2, 100);
+  playerPaddle = new Paddle(canvasWidth - 17, 100, context)
+  ball = new Ball(canvasWidth / 2, canvasHeight / 2, context);
 	
-  playerPaddle = new Paddle(canvasWidth - 17, 100)
-	
-  ball = new Ball(canvasWidth / 2, canvasHeight / 2);
-	
-  render();
+  render(context, canvasWidth, canvasHeight);
 }
 
 
-var render = function(){
-	cpuPaddle.render(2, 100);
-	playerPaddle.render(canvasWidth - 17, 220)
-	ball.render(canvasWidth / 2, canvasHeight /  2);
-}
+
 
 
 var animate = window.requestAnimationFrame ||
@@ -79,7 +78,7 @@ var step = function(){
 
 }
 
-window.onload = setElements;
+
 
 
 
